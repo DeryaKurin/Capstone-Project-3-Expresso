e5228a061e64154c7d731cb28917d429a70408ee
Derya Ozdemir <deryaokurin@Deryas-MBP.fios-router.home>
2018-10-25 15:32:15 -0400

    third commit

--- api/employee.js
+++ api/employee.js
@@ -30,40 +30,41 @@ employeesRouter.get('/', (req, res, next) => {
     });
 });
 
+employeesRouter.post('/', (req, res, next) => {
+  const name = req.body.employee.name;
+  const position = req.body.employee.position;
+  const wage = req.body.employee.wage;
+  const is_current_employee = req.body.employee.is_current_employee = 0 ? 0 : 1;
+
+  if(!name, !position, !wage) {
+    return res.sendStatus(400);
+  }
+
+  const sql = 'INSERT INTO Employee (name, position, wage, is_current_employee) VALUES ($name, $position, $wage, $isCurrentEmployee)';
+  const values = {
+    $name: req.body.employee.name,
+    $position: req.body.employee.position,
+    $wage: req.body.employee.wage,
+    $isCurrentEmployee: req.body.employee.isCurrentEmployee
+  };
+
+  db.run(sql, values, function(error) {
+    if (error) {
+      next(error);
+    } else {
+      db.get(`SELECT * FROM Employee WHERE Employee.id = ${this.lastID}`,
+        (error, employee) => {
+          res.status(201).json({employee: employee});
+        });
+    }
+  });
+});
+
 
 employeesRouter.get('/:employeeId', (req, res, next) => {
   res.status(200).json({employee: req.employee});
 });
 
-// employeesRouter.post('/', (req, res, next) => {
-//   const name = req.body.employee.name;
-//   const position = req.body.employee.position;
-//   const wage = req.body.employee.wage;
-//   const is_current_employee = req.body.employee.isCurrentEmployee = 0 ? 0 : 1;
-//
-//   if(!name, !position, !wage) {
-//     return res.sendStatus(400);
-//   }
-//
-//   const sql = 'INSERT INTO Employee (name, position, wage, is_current_employee) VALUES ($name, $position, $wage, $isCurrentEmployee)';
-//   const values = {
-//     $name: name,
-//     $position: position,
-//     $wage: wage,
-//     $isCurrentEmployee: isCurrentEmployee
-//   };
-//
-//   db.run(sql, values, function(error) {
-//     if (error) {
-//       next(error);
-//     } else {
-//       db.get(`SELECT * FROM Employee WHERE Employee.id = ${this.lastID}`,
-//         (error, employee) => {
-//           res.status(201).json({employee: employee});
-//         });
-//     }
-//   });
-// });
 
 employeesRouter.put('/:employeeId', (req, res, next) => {
   const name = req.body.employee.name;
